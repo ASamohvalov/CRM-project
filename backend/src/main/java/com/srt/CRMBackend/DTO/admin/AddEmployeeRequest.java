b556e728bce3196c51876754c70b1e723794fad1
artem <artym.samohvaloff@gmail.com>
2025-03-13 16:41:33 +0400

    Fix add employee request

--- backend/src/main/java/com/srt/CRMBackend/DTO/admin/AddEmployeeRequest.java
+++ backend/src/main/java/com/srt/CRMBackend/DTO/admin/AddEmployeeRequest.java
@@ -3,11 +3,13 @@ package com.srt.CRMBackend.DTO.admin;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@Builder
 public class AddEmployeeRequest {
     @NotBlank
     private String login;
